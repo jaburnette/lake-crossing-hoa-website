b8a63e6c242da51764042dbe2fc6b0e1e22947b1
Justin Burnette <535077+jaburnette@users.noreply.github.com>
2024-08-20 11:30:37 -0400

    Deploy on content updates

--- src/contentful.js
+++ src/contentful.js
@@ -3,5 +3,5 @@ import contentful from "contentful";
 export const contentfulClient = contentful.createClient({
     space: import.meta.env.CONTENTFUL_SPACE_ID,
     accessToken: import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-    host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+    host: "cdn.contentful.com",
 });
