af0c4e1ce6d0541e6df8e1ab2d1a8e5e82bac329
Justin Burnette <535077+jaburnette@users.noreply.github.com>
2024-08-21 16:30:05 -0400

    Allow dynamic page creation, allow body image rendering

--- src/contentful.js
+++ src/contentful.js
@@ -1,7 +1,17 @@
 import contentful from "contentful";
+import {documentToHtmlString} from "@contentful/rich-text-html-renderer";
 
 export const contentfulClient = contentful.createClient({
     space: import.meta.env.CONTENTFUL_SPACE_ID,
     accessToken: import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
     host: "cdn.contentful.com",
 });
+
+// this is just a helper function that lets us handle images inside the body content
+export const renderBodyContent = body => {
+    return documentToHtmlString(body, {
+        renderNode: {
+            ["embedded-asset-block"]: (node) => `<img src="${node.data.target.fields.file.url}" class="my-6 rounded max-w-full" />`,
+        }
+    })
+};
